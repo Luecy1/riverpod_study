595c27b1ba1397745b66d7c53eef90f2f79180c6
Youhei Oonuma <oonuma617@gmail.com>
2022-05-08 00:22:28 +0900

    provider

--- lib/riverpod.dart
+++ lib/riverpod.dart
@@ -3,3 +3,9 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 final provider = Provider((ref) {
   return 'HELLO';
 });
+
+final futureProvider = FutureProvider(
+  (ref) {
+    return Future.delayed(const Duration(seconds: 1), () => 'DELAYED ');
+  },
+);
